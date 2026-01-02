269e52844f901655688311dd8120384889b2fbee
Osman.ordu <orduosmann@gmail.com>
2026-01-02 17:49:49 +0300

    main | Implemented Tabs

--- src/feautures/transactions/screens/TransactionsScreen.tsx
+++ src/feautures/transactions/screens/TransactionsScreen.tsx
@@ -1,17 +1,40 @@
 import React from 'react';
+import { View } from 'react-native';
 import { ScreenLayout } from '@/components/layout';
-import { ThemedView } from '@/components/ui/themed-view';
+import { Tabs, type TabItem } from '@/components/ui';
+import AllTransactions from '../components/AllTransactions';
 import CreateTransaction from '../components/CreateTransaction';
 import QuickTransactions from '../components/LastTransactions';
 import { styles } from './TransactionsScreen.styles';
 
 export default function TransactionsScreen() {
+  const tabs: TabItem[] = [
+    {
+      value: 'create',
+      label: 'İşlem Oluştur',
+      content: (
+        <View style={styles.createTransactionContainer}>
+          <CreateTransaction />
+        </View>
+      ),
+    },
+    {
+      value: 'all',
+      label: 'Tüm İşlemler',
+      content: <AllTransactions />,
+    },
+    {
+      value: 'recent',
+      label: 'Son İşlemler',
+      content: <QuickTransactions />,
+    },
+  ];
+
   return (
     <ScreenLayout scrollContentStyle={styles.scrollContent}>
-      <ThemedView style={styles.content}>
-        <CreateTransaction />
-        <QuickTransactions />
-      </ThemedView>
+      <View style={styles.container}>
+        <Tabs tabs={tabs} defaultTab="create" />
+      </View>
     </ScreenLayout>
   );
 }
